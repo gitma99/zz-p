6d0f1bccfe7171d6983c3d39487fac26ce831d2f
gitma99 <145668334+gitma99@users.noreply.github.com>
2023-09-22 17:09:20 +0330

    Add files via upload

---
+++ backup_service.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+
+
+SERVICE_NAME="zz-backup"
+SCRIPT_PATH="/var/www/html/ZanborPanelBot/telegram_schedual_backup.sh"
+LOG_PATH="/var/www/html/ZanborPanelBot/backup.log"
+default_telegram_config_json="/var/www/html/ZanborPanelBot/DEFAULTS/backup_service_config.json"
+telegram_config_json="/var/www/html/ZanborPanelBot/backup_service_config.json"
+
+if [[ $EUID -ne 0 ]]; then
+echo "This script must be run as root."
+exit 1
+fi
+
+
+
+# Add a cron job to run a script every day at midnight
+
+create_service(){
+cat <<EOF > "/etc/systemd/system/$SERVICE_NAME.service"
+[Unit]
+Description=My Bash Script Service
+After=network.target
+
+[Service]
+ExecStart=$SCRIPT_PATH
+Restart=always
+StandardOutput=append:$LOG_PATH
+StandardError=append:$LOG_PATH
+
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+colorized_echo() {
+    local color=$1
+    local text=$2
+    
+    case $color in
+        "red")
+        printf "\e[91m${text}\e[0m\n";;
+        "green")
+        printf "\e[92m${text}\e[0m\n";;
+        "yellow")
+        printf "\e[93m${text}\e[0m\n";;
+        "blue")
+        printf "\e[94m${text}\e[0m\n";;
+        "magenta")
+        printf "\e[95m${text}\e[0m\n";;
+        "cyan")
+        printf "\e[96m${text}\e[0m\n";;
+        *)
+            echo "${text}"
+        ;;
+    esac
+}
+
+colorized_echo green "\n[+] - Please wait for a few seconde !"
+
+echo " "
+
+question="Please select your action?"
+actions=("Create Backup Task" "Delete Backup Task" "Exit")
+
+
+select opt in "${actions[@]}"
+do
+    case $opt in 
+        "Create Backup Task")
+            colorized_echo cyan "Enter the Token :"
+            printf "" 
+            read TOKEN
+            colorized_echo cyan "Enter the chat id number :" 
+            printf "" 
+            read CHAT_ID
+
+            default_file=$default_telegram_config_json
+            destination_file="$default_telegram_config_json.tmp"
+            replace=$(cat "$default_file" | sed -e "s/\[\*TOKEN\*\]/${TOKEN}/g" -e "s/\[\*CHAT\*\]/${CHAT_ID}/g")
+            echo "$replace" > "$destination_file"
+            mv "$destination_file" "$telegram_config_json"
+
+            # Create the systemd service unit file
+            create_service
+
+            # Reload systemd
+            systemctl daemon-reload
+
+            # Enable and start the service
+            systemctl enable "$SERVICE_NAME.service"
+            systemctl start "$SERVICE_NAME.service"
+
+            # Check the status
+            systemctl status "$SERVICE_NAME.service"
+
+            echo "Service $SERVICE_NAME has been created and started."
+
+
+        break;;
+        "Delete Backup Task")
+
+            # Stop and disable the service
+            systemctl stop "$SERVICE_NAME.service"
+            systemctl disable "$SERVICE_NAME.service"
+
+            # Remove the service unit file
+            rm "/etc/systemd/system/$SERVICE_NAME.service"
+
+            # Reload systemd
+            systemctl daemon-reload
+
+            echo "Service $SERVICE_NAME has been disabled and removed."
+
+        break;;
+        "Exit")
+            echo -e "\n"
+            colorized_echo green "Exited !"
+            echo -e "\n"
+
+            break;;
+        *) 
+            echo "Invalid option!"
+
+        break;;
+    esac
+done
---
+++ telegram_schedual_backup.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+target_time_zone='Asia/Tehran'
+CONFIG_JSON='/var/www/html/ZanborPanelBot/backup_service_config.json'
+
+telegram_config_content=$(cat $CONFIG_JSON)
+BOT_TOKEN=$(echo "$telegram_config_content" | jq -r '.token')
+USER_ID=$(echo "$telegram_config_content" | jq -r '.chat_id')
+
+colorized_echo() {
+    local color=$1
+    local text=$2
+    
+    case $color in
+        "red")
+        printf "\e[91m${text}\e[0m\n";;
+        "green")
+        printf "\e[92m${text}\e[0m\n";;
+        "yellow")
+        printf "\e[93m${text}\e[0m\n";;
+        "blue")
+        printf "\e[94m${text}\e[0m\n";;
+        "magenta")
+        printf "\e[95m${text}\e[0m\n";;
+        "cyan")
+        printf "\e[96m${text}\e[0m\n";;
+        *)
+            echo "${text}"
+        ;;
+    esac
+}
+
+send_backup(){
+        if [ -d "/var/www/html/ZanborPanelBot" ]; then
+        if [ -f "/var/www/html/ZanborPanelBot/bot_config.json" ]; then
+            if [ -s "/var/www/html/ZanborPanelBot/bot_config.json" ]; then
+
+                colorized_echo green "Please wait, Backuping . . ."
+                content=$(cat "/var/www/html/ZanborPanelBot/bot_config.json")
+                db_name=$(echo "$content" | jq -r '.database.db_name')
+                db_username=$(echo "$content" | jq -r '.database.db_username')
+                db_password=$(echo "$content" | jq -r '.database.db_password')
+                mysqldump -u $db_username -p$db_password $db_name > /root/zz-p-sql.sql
+                if [ -f "/root/zz-p-sql.sql" ]; then
+                    zip /root/zz-p-backup.zip /var/www/html/ZanborPanelBot/bot_config.json /root/zz-p-sql.sql
+                    if [ -f "/root/zz-p-backup.zip" ]; then
+                        colorized_echo green "Backup Finshed. located in (/root/zz-p-baclup.zip)."
+                        
+                        curl -X POST \
+                            -F "document=@/root/zz-p-backup.zip" \
+                            -F "chat_id=$USER_ID" \
+                            https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+
+                    else
+                        colorized_echo red "backup Failed. (Cant create zip file)"
+                    fi
+                else
+                    colorized_echo red "backup Failed. (Cant backup mysql data..)"
+
+                fi
+            else
+                echo -e "\n"
+                colorized_echo red "The bot_config.json file is empty!"
+                echo -e "\n"
+                exit 1
+            fi
+        else
+            echo -e "\n"
+            colorized_echo red "The bot_config.json file was not found and the backup process was canceled!"
+            echo -e "\n"
+            exit 1
+        fi
+    else
+        echo -e "\n"
+        colorized_echo red "The ZanborPanelBot folder was not found for the backup process, install the bot first!"
+        echo -e "\n"
+        exit 1
+    fi
+}
+
+
+midnight_seconds=$(TZ="$target_time_zone" date -d '00:00 tomorrow' +%s)
+
+while true; do
+    send_backup 
+
+    current_time=$(TZ="$target_time_zone" date +%H:%M)
+    current_seconds=$(TZ="$target_time_zone" date -d "$current_time" +%s)
+    time_remaining=$(($midnight_seconds - $current_seconds))
+
+    echo "\n"
+    echo "Chron Info : $time_remaining secconds until next backup!"
+    sleep $time_remaining
+done
